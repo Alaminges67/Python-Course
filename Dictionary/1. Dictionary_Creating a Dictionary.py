c551482d581eaf9e9e123f28aa31c88cb178bc43
Md. Al Amin <157206524+Alaminges67@users.noreply.github.com>
2024-11-20 08:06:36 +0000

    Practice Lists

--- Dictionary/1. Dictionary_Creating a Dictionary.py
+++
@@ -1,9 +0,0 @@
-# Example 1: Creating a Dictionary
-# A dictionary is a collection of key-value pairs, created using curly braces {}.
-# Each key is unique, and it is used to access its corresponding value.
-
-# Creating a dictionary with various key-value pairs
-my_dict = {'student_name': 'Al_Amin', 'age': 24, 'roll': 102}
-
-# Printing the created dictionary
-print('A student information:', my_dict)
